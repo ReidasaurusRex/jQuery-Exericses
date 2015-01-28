f38be4f40a50998557fbf0c4f7668733d18028a5
ReidasaurusRex <reidhlewis91@gmail.com>
2015-01-27 18:35:46 -0800

    Finished exercises, no bonus, ex3 has weird dom jump due to hide.

--- ex3/app.js
+++ ex3/app.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
-	$(window).on("keyup", function() {
+	$("h3").hide();
+	$(window).on("keyup", function(e) {
 		if ($("input[type='password']").val().length < 6) {
-			$("p").text("Not long enough!");
+			$("p:first").text("Not long enough!");
 		}
 		if ($("input[type='password']").val().length >= 6) {
-			$("p").text("");
+			$("p:first").text("");
+		}
+		if (e.keyCode !== 13) {
+			$("#error").text("");
+		}
+	});
+	$("form").on("submit", function(e) {
+		if (($("input[type='password']").val().length < 6) && ($("input[type='password']").val().length >= 1)) {
+			$("#error").text("Your password must be longer than 6 characters.");
+		}
+		else if ($("input[type='password']").val().length === 0) {
+			$("#error").text("Please enter a password");
+		}
+		else if ($("input[type='password']").val().length >= 6) {
+			$("h3").fadeIn(500);
 		}
 	});
 });
